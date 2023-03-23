ae4ed7e72b9f01a8c77b8af11e79433f586bd8b0
sadeline <42850722+stephadeline@users.noreply.github.com>
2023-03-24 02:13:29 +0700

    add raster

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -2,6 +2,10 @@
   import allTollRoadData from "./../data/java-toll-data.json";
 
   import restArea from "./../data/rest-area.json";
+
+  import tollGates from "./../data/gerbang-tol.json";
+
+  import underConstruction from "./../data/konstruksi.json";
   import contentData from "./../data/test-content-2.json";
 
   import { onMount, onDestroy } from "svelte";
@@ -16,7 +20,10 @@
   let mapContainer;
   let slideContent;
 
-  let allLayerId = ['rest-areas']
+  let allLayerId = ["rest-areas", "toll-gates"];
+  let allLinesId = ["construction"]
+  let allRaster = ["brebes-2014", "brebes-2020"]
+
 
   onMount(() => {
     const initialState = { lng: 110.5, lat: -6.8, zoom: 6.5 };
@@ -39,6 +46,52 @@
         data: restArea,
       });
 
+      map.addSource("toll-gates", {
+        type: "geojson",
+        data: tollGates,
+      });
+
+      map.addSource("construction", {
+        type: "geojson",
+        data: underConstruction,
+      });
+
+      map.addSource("brebes-2014", {
+        "type": "image",
+        "url": "assets/brebes2014.jpg",
+        "coordinates": [
+          [ 108.296246230602279, -6.626304532490591 ],[ 109.394879043102264, -6.626304532490591 ], [ 109.394879043102264, -7.096013863041794 ], [ 108.296246230602279, -7.096013863041794 ],
+        ]
+    });
+
+    map.addSource("brebes-2020", {
+        "type": "image",
+        "url": "assets/brebes-2020.jpg",
+        "coordinates": [
+           [ 108.3480128645897, -6.6200220929638665 ],
+           [ 109.4466456770897, -6.6200220929638665 ],
+          [ 109.4466456770897, -7.128575515704274 ], [ 108.3480128645897, -7.128575515704274 ],
+        ]
+    });
+
+    map.addLayer({
+    "id": "brebes-2014",
+    "source": "brebes-2014",
+    "type": "raster",
+    "paint": {
+        "raster-opacity": 0
+    }
+});
+
+map.addLayer({
+    "id": "brebes-2020",
+    "source": "brebes-2020",
+    "type": "raster",
+    "paint": {
+        "raster-opacity": 0
+    }
+});
+
       map.addLayer({
         id: "all_toll_roads",
         type: "line",
@@ -50,19 +103,42 @@
         paint: {
           "line-color": "#FF8200",
           "line-width": 3,
-          
         },
       });
       map.addLayer({
-        'id': 'rest-areas',
-        'type': 'circle',
-        'source': 'rest-areas',
+        id: "rest-areas",
+        type: "circle",
+        source: "rest-areas",
         paint: {
           "circle-color": "#06BCC1",
-          "circle-opacity": 0
+          "circle-opacity": 0,
         },
-        });
+      });
 
+      map.addLayer({
+        id: "toll-gates",
+        type: "circle",
+        source: "toll-gates",
+        paint: {
+          "circle-color": "#ef4123",
+          "circle-opacity": 0,
+        },
+      });
+
+      map.addLayer({
+        id: "construction",
+        type: "line",
+        source: "construction",
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "#FF8200",
+          "line-width": 3,
+          "line-dasharray": [0.1, 2],
+        },
+      });
     });
   });
   onDestroy(() => {
@@ -78,43 +154,75 @@
     });
 
     if (slideContent.tolls_filter) {
-
-    map.setFilter('all_toll_roads', slideContent.tolls_filter)
+      map.setFilter("all_toll_roads", slideContent.tolls_filter);
     } else {
-      map.setFilter('all_toll_roads', ['has', 'yearnum'])
+      map.setFilter("all_toll_roads", ["has", "yearnum"]);
+    }
 
+    if (slideContent.show_tolls === "FALSE") {
+      map.setPaintProperty("all_toll_roads", "line-opacity", 0)
+    } else {
+      map.setPaintProperty("all_toll_roads", "line-opacity", 1)
     }
 
     if (slideContent.other_layers.length > 0) {
-
-      allLayerId.forEach(layer => {
-        if (slideContent.other_layers.includes(layer)){
-          map.setPaintProperty(layer, 'circle-opacity', 1)
+      allLayerId.forEach((layer) => {
+        if (slideContent.other_layers.includes(layer)) {
+          map.setPaintProperty(layer, "circle-opacity", 1);
         } else {
-        map.setPaintProperty(layer, 'circle-opacity', 0)
+          map.setPaintProperty(layer, "circle-opacity", 0);
         }
-      })
+      });
     } else {
       // remove all layers
-      allLayerId.forEach(layer => {
-        map.setPaintProperty(layer, 'circle-opacity', 0)
-      })
+      allLayerId.forEach((layer) => {
+        map.setPaintProperty(layer, "circle-opacity", 0);
+      });
+    }
+
+    if (slideContent.other_line_layers.length > 0) {
+      allLinesId.forEach((layer) => {
+        if (slideContent.other_line_layers.includes(layer)) {
+          map.setPaintProperty(layer, "line-opacity", 1);
+        } else {
+          map.setPaintProperty(layer, "line-opacity", 0);
+        }
+      });
+    } else {
+      // remove all layers
+      allLinesId.forEach((layer) => {
+        map.setPaintProperty(layer, "line-opacity", 0);
+      });
+    }
+
+    if (slideContent.raster_layers.length > 0) {
+      allRaster.forEach((layer) => {
+        if (slideContent.raster_layers.includes(layer)) {
+          map.setPaintProperty(layer, "raster-opacity", 1);
+        } else {
+          map.setPaintProperty(layer, "raster-opacity", 0);
+        }
+      });
+    } else {
+      // remove all layers
+      allRaster.forEach((layer) => {
+        map.setPaintProperty(layer, "raster-opacity", 0);
+      });
     }
 
     if (slideContent.style) {
-      map.setPaintProperty(slideContent.style_layer, slideContent.style_property, slideContent.style)
+      map.setPaintProperty(
+        slideContent.style_layer,
+        slideContent.style_property,
+        slideContent.style
+      );
     }
     if (selectedYear) {
-    if (slideContent.slider === "TRUE") {
-      map.setFilter('all_toll_roads', ["<=", "yearnum", selectedYear])
+      if (slideContent.slider === "TRUE") {
+        map.setFilter("all_toll_roads", ["<=", "yearnum", selectedYear]);
+      }
     }
   }
-
-  }
-
-
-
-
 </script>
 
 <div class="map-wrap">
@@ -125,7 +233,7 @@
   .map-wrap {
     position: relative;
     width: 100%;
-    height: 100vh /* calculate height of the screen minus the heading */
+    height: 100vh; /* calculate height of the screen minus the heading */
   }
   .map {
     position: absolute;
