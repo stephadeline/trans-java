50fec90f26117e1990e4958c5aef2673e891739c
sadeline <42850722+stephadeline@users.noreply.github.com>
2023-04-06 17:09:42 +0700

    final readme edit

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -10,6 +10,7 @@
   import pantura from "./../data/pantura-brebes.json";
   import contentData from "./../data/content.json";
   import desa from "./../data/desa.json";
+  
   import { onMount, onDestroy } from "svelte";
   import maplibregl from "maplibre-gl";
   import "maplibre-gl/dist/maplibre-gl.css";
@@ -397,11 +398,6 @@
 
     if (slideContent.rest_area_filters) {
       map.setFilter("rest-areas", slideContent.rest_area_filters);
-      // map.setLayoutProperty('rest-areas', 'text-field', [
-      // 'format',
-      // ['get', 'keterangan'],
-      // { 'font-scale': 1.2 },
-      // ]);
     } else {
       map.setFilter("rest-areas", ["has", "lat"]);
     }
@@ -508,15 +504,9 @@
           atm +
           gas;
 
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
         while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
           coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
         }
-
-        // Populate the popup and set its coordinates
-        // based on the feature found.
         popupRestArea.setLngLat(coordinates).setHTML(description).addTo(map);
       });
 
